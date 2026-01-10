6dd33c5243a8578fc40f02412a11711d95f37c53
intannajwa20 <s22a0085@siswa.umk.edu.my>
2026-01-11 00:45:07 +0800

    Update page1_memberA.py

--- pages/page1_memberA.py
+++ pages/page1_memberA.py
@@ -71,8 +71,11 @@ st.caption(
 st.markdown("""
 **Key Insight:**  
 *This chart shows the average frequency of study techniques used by students.
+
 *From the chart, we can see that students most frequently use summarising notes, watching videos, doing practice exercises, and reading notes.
+
 *In contrast, techniques such as flashcards and teaching others are used less often.
+
 *This suggests that students prefer familiar and independent study methods rather than more active or collaborative techniques.”.
 """)
 
